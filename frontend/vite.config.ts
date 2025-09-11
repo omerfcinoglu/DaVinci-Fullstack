b16f79b7f2591da1a8b96370f90e3a14d49ca490
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-09-11 18:16:12 +0300

    Migrate frontend to Vite & NextUI template

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+  base: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["@heroui/react", "@heroui/button"],
+  },
+});
