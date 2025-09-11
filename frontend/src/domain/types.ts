3d9d241724d4c18dfaefae6d5341206b7c455806
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2025-09-11 20:14:57 +0300

    Update types.ts

--- frontend/src/domain/types.ts
+++ frontend/src/domain/types.ts
@@ -4,3 +4,11 @@ export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
 
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+};
+
+export type Post = { userId: number; id: number; title: string; body?: string };
